98d647b346f99fbe5cd91c8494f2341db1af4b86
Jyotsna <jp29feb@gmail.com>
2026-07-15 12:18:17 +0200

    Get LuckyNumber

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("HI!");
 
 var provider = new LocalDateTimeProvider();
 Console.WriteLine($"Current Date and Time: {provider.GetCurrentDateTime()}");
+
+var luckynumberProvider = new RandomNumberProvider();
+Console.WriteLine($"Your lucky number is: {luckynumberProvider.GetLuckyNumber()}");
---
+++ HelloWorldApp/RandomNumberProvider.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Security.Cryptography;
+
+public class RandomNumberProvider
+{
+    public RandomNumberProvider()
+    {
+    }
+
+    public string GetLuckyNumber()
+    {
+        var randomNumber = RandomNumberGenerator.GetInt32(1, 11);
+        return randomNumber.ToString();
+    }
+}
