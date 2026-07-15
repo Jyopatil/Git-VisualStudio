4d0e55c616058ddb859bd8d2a5e614f73a6b0566
Jyotsna <jp29feb@gmail.com>
2026-07-15 12:03:42 +0200

    added Time

---
+++ HelloWorldApp/LocalDateTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    public class LocalDateTimeProvider :IDateTimeProvider
+    {
+        public string GetCurrentDateTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿
+using HelloWorldApp;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("HI!");
+
+var provider = new LocalDateTimeProvider();
+Console.WriteLine($"Current Date and Time: {provider.GetCurrentDateTime()}");
